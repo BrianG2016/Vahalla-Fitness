fbef97daf5ef4bb92c63856e00cce57ff110a26d
BrianG2016 <bj_gaskamp86@hotmail.com>
2025-10-21 18:50:14 -0500

    Delete db.js

--- db.js
+++
@@ -1,2 +0,0 @@
-import mongoose from 'mongoose'
-export async function connectDB(){ const uri=process.env.MONGODB_URI; if(!uri){console.warn('No MONGODB_URI'); return} await mongoose.connect(uri); console.log('🗿 Mongo connected') }
