1c070faef158a69ef569cad5a69bae5ace3c6af6
BrianG2016 <bj_gaskamp86@hotmail.com>
2025-10-23 18:15:22 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import 'dotenv/config'
+import paymentRoutes from './routes/payments.js'
+app.use('/api/payments', paymentRoutes)import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 
